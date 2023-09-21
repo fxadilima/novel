d7d7ada7a1a9db103cc065ae73ddf940d1e19843
fxadilima <xaverius2017@gmail.com>
2023-09-21 12:36:39 +0700

    server entry

--- server.ts
+++ server.ts
@@ -1,7 +1,7 @@
 import Server from "lume/core/server.ts";
 const server = new Server({
 port: 8000,
-root: `${Deno.cwd()}/_site`,
+root: `${Deno.cwd()}`,
 });
 server.start();
 console.log("Listening on http://localhost:8000");
